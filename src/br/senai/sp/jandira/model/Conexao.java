d33b68bfb60e19e70db8a2f4d949898cbfcc8cc1
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-12-05 16:52:47 -0300

    ProjetoFinal

---
+++ src/br/senai/sp/jandira/model/Conexao.java
@@ -0,0 +1,34 @@
+package br.senai.sp.jandira.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private String servidor, banco, usuario, senha;
+    public Connection conexao;
+
+    public Conexao (){
+
+        this.servidor = "localhost";
+        this.banco = "db_contatos";
+        this.usuario = "root";
+        this.senha = "bcd127";
+
+    }
+
+    public void connectDrive(){
+        try {
+            this.conexao = DriverManager.getConnection("jdbc:mysql://" + this.servidor + "/" + this.banco, this.usuario, this.senha);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public Connection getConnection (){
+        connectDrive();
+        return conexao;
+    }
+}
